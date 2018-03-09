87d25ff7ae3d377bdfc4fd5d3d5438b39b72b153
Nikhil Kilari <36819773+kilarinikhil@users.noreply.github.com>
2018-03-10 00:16:58 +0530

    Wanna read and write on the image

--- Open-CV/third.py
+++ Open-CV/third.py
@@ -1,16 +1,19 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('kohli.jpg', cv2.IMREAD_COLOR)
+img = cv2.imread('kohli.jpg',cv2.IMREAD_COLOR)
 
-pts=np.array([[20,5],[20,20],[30,20],[30,5]],np.int32)
+cv2.line(img, (0,0),(150,150),(0,0,255),10) # args:(img, start, end, color, thickness)
+cv2.rectangle(img, (15,20),(350,400),(255,0,0),5) # args:(img, topleft, bottomright, color, thickness)
+cv2.circle(img, (150,150),100, (0,255,0), 10) # args:(img, center, radius, color, thickness) thickness of -1 gives filled solid fig
 
-cv2.polylines(img,[pts],True,(30,30,30),2)
+pts = np.array([[100,50],[200,30],[70,200],[500,10]],np.int32)
+pts = pts.reshape((-1,1,2))
+cv2.polylines(img,[pts],True,(0,255,255),5) #args : (img, coord array, True (closed)/False ,color,thickness) 
 
-font=cv2.FONT_HERSHEY_SIMPLEX
-cv2.putText(img,'KING KOHLI!',(200,200),font,1,(70,70,70),2,cv2.LINE_AA)
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img,'OpenCV',(250,350),font,2,(23,3,45),2,cv2.LINE_AA) #args : (img,text,start pos,font,size,color,thickness,line_type)
 
-cv2.imshow('kohli',img)
+cv2.imshow('image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
- 
