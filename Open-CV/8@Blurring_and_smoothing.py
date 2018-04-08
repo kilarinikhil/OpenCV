2944dc12eb4603fe5162ffa29fa5779f87f5697d
Nikhil Kilari <36819773+kilarinikhil@users.noreply.github.com>
2018-04-08 12:25:28 +0530

    Update 8@Blurring_and_smoothing.py

--- Open-CV/8@Blurring_and_smoothing.py
+++ Open-CV/8@Blurring_and_smoothing.py
@@ -8,13 +8,13 @@ while True:
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     lower_red = np.array([0,0,0])
-    upper_red = np.array([255,255,255])
+    upper_red = np.array([50,220,240])#Defining the upper limits for lemon yellow
 
     mask = cv2.inRange(hsv, lower_red, upper_red)
     res = cv2.bitwise_and(frame, frame, mask = mask)
 
-    kernal = np.ones((15,15),np.float32)/225#Defining the filter
-    smoothed = cv2.filter(res,-1,kernel)#Applying the filter
+    kernel = np.ones((15,15),np.float32)/225#Defining the filter
+    smoothed = cv2.filter2D(res,-1,kernel)#Applying the filter
 
     #Gaussian Blur
     blur = cv2.GaussianBlur(res, (15,15),0) 
@@ -25,13 +25,14 @@ while True:
     #Bilateral Blur
     bilateral = cv2.bilateralFilter(res,15,75,75)
     
-    cv2.imshow('frame', frame)
+    cv2.imshow('frame', frame)#The actual feed to the camera
+    ##Uncommen the desired output among all the median filter is the best.
     ##cv2.imshow('mask', mask)
-    cv2.imshow('res', res)
+    ##cv2.imshow('res', res)
     ##cv2.imshow('smoothed', smoothed)
     ##cv2.imshow('blur', blur)
     ##cv2.imshow('median', median)
-    ##cv2.imshow('bilateral', bilateral)
+    cv2.imshow('bilateral', bilateral)
 
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
