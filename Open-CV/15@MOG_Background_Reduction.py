ff218ca8f5c44dacaa25d27eb00ec308031a6d64
Nikhil Kilari <36819773+kilarinikhil@users.noreply.github.com>
2018-06-13 21:58:34 +0530

    Update 15@MOG_Background_Reduction.py

--- Open-CV/15@MOG_Background_Reduction.py
+++ Open-CV/15@MOG_Background_Reduction.py
@@ -1,7 +1,9 @@
+##All the moving objects will be displayed here
+##This is better to be called as no-action reduction than background reduction
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) ##You can either use the cam or enter your video address here. 
 fgbg = cv2.createBackgroundSubtractorMOG2()
 
 while True:
